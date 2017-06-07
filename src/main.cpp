8a4edf28895ea28f06fa6fd5c16977319427da91
dmbelo <david.belo@mclaren.com>
2017-06-07 15:23:33 -0700

    Spring cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,9 @@
 #include "MPC.h"
 #include "Poly.h"
 #include "json.hpp"
-#include "matplotlibcpp.h"
 
 // for convenience
 using json = nlohmann::json;
-
-namespace plt = matplotlibcpp;
 
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
@@ -24,13 +21,13 @@ double rad2deg(double x) { return x * 180 / pi(); }
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
-string hasData(string s) {
+std::string hasData(std::string s) {
   auto found_null = s.find("null");
   auto b1 = s.find_first_of("[");
   auto b2 = s.rfind("}]");
-  if (found_null != string::npos) {
+  if (found_null != std::string::npos) {
     return "";
-  } else if (b1 != string::npos && b2 != string::npos) {
+  } else if (b1 != std::string::npos && b2 != std::string::npos) {
     return s.substr(b1, b2 - b1 + 2);
   }
   return "";
@@ -47,71 +44,42 @@ int main() {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
-    string sdata = string(data).substr(0, length);
-    cout << sdata << endl;
+    std::string sdata = std::string(data).substr(0, length);
+    // std::cout << sdata << std::endl;
     if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
-      string s = hasData(sdata);
+      std::string s = hasData(sdata);
       if (s != "") {
         auto j = json::parse(s);
-        string event = j[0].get<string>();
+        std::string event = j[0].get<std::string>();
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          std::vector<double> ptsx = j[1]["ptsx"];
-          std::vector<double> ptsy = j[1]["ptsy"];
-          double px = j[1]["x"];
-          double py = j[1]["y"];
-          double psi = j[1]["psi"];
-          double v = j[1]["speed"];
+          std::vector<double> x_waypts = j[1]["ptsx"];
+          std::vector<double> y_waypts = j[1]["ptsy"];
+          double x_car = j[1]["x"];
+          double y_car = j[1]["y"];
+          double a_heading = j[1]["psi"];
+          double v_car = j[1]["speed"];
 
-
-          vector<double> ptsx_v, ptsy_v;
+          std::vector<double> xv_waypts, yv_waypts;
           // Convert desired trajectory waypoints to vehicle coordinate system
-          for (unsigned int i = 0; i < ptsx.size(); i++) {
+          for (unsigned int i = 0; i < x_waypts.size(); i++) {
 
-            double dx = ptsx[i] - px;
-            double dy = ptsy[i] - py;
+            double dx = x_waypts[i] - x_car;
+            double dy = y_waypts[i] - y_car;
             double mag = sqrt(dx * dx  + dy * dy);
             double theta = atan2(dy, dx);
 
-            ptsx_v.push_back(mag * cos(psi - theta));
-            ptsy_v.push_back(-mag * sin(psi - theta));
+            xv_waypts.push_back(mag * cos(a_heading - theta));
+            yv_waypts.push_back(-mag * sin(a_heading - theta));
             
           }
 
           // Polynomial fitting
-          Eigen::Map<Eigen::VectorXd> x_vals(&ptsx_v[0], ptsx_v.size());
-          Eigen::Map<Eigen::VectorXd> y_vals(&ptsy_v[0], ptsy_v.size());
-          poly.Fit(x_vals, y_vals, 3);
-
-
-          // Uncomment the following for debug plotting
-          // std::vector<double> vx_vector;
-          // std::vector<double> vy_vector;
-
-          // {
-
-          //   double a = 10;
-          //   // Origin end of vector
-          //   vx_vector.push_back(px);
-          //   vy_vector.push_back(py);
-          //   // Termination end of vector
-          //   vx_vector.push_back(px + a * cos(psi));
-          //   vy_vector.push_back(py + a * sin(psi));
-            
-          // };
-          
-
-          // plt::subplot(1, 2, 1);
-          // plt::title("Map Coordinate System");
-          // plt::plot(ptsx, ptsy, "o");
-          // plt::plot(vx_vector, vy_vector);
-          // plt::plot({vx_vector[0]}, {vy_vector[0]}, "*");
-
-          // plt::subplot(1, 2, 2);
-          // plt::title("Vehicle Coordinate System");
-          // plt::plot(ptsx_v, ptsy_v, "o", x_poly, y_poly);
-
-          // plt::show();
+          {
+            Eigen::Map<Eigen::VectorXd> x(&xv_waypts[0], xv_waypts.size());
+            Eigen::Map<Eigen::VectorXd> y(&yv_waypts[0], yv_waypts.size());
+            poly.Fit(x, y, 3);
+          }
 
           /*
           * TODO: Calculate steering angle and throttle using MPC.
@@ -119,49 +87,43 @@ int main() {
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
+          double a_steer;
+          double r_throttle;
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = throttle_value;
+          msgJson["steering_angle"] = a_steer;
+          msgJson["throttle"] = r_throttle;
 
           //Display the MPC predicted trajectory 
-          std::vector<double> mpc_x_vals;
-          std::vector<double> mpc_y_vals;
+          std::vector<double> xv_pred_traj;
+          std::vector<double> yv_pred_traj;
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
-
-          // // Polynomial evaluation
-          // std::vector<double> x_poly;
-          // std::vector<double> y_poly;
-
-          // for (double x = 0; x <= 50; x += 1.0) {
-
-          //   auto v = poly.Eval(x);
-          //   x_poly.push_back(x);
-          //   y_poly.push_back(v);
-
-          // }
-
-
-          msgJson["mpc_x"] = mpc_x_vals;
-          msgJson["mpc_y"] = mpc_y_vals;
+          msgJson["mpc_x"] = xv_pred_traj;
+          msgJson["mpc_y"] = yv_pred_traj;
 
           //Display the waypoints/reference line
-          // vector<double> next_x_vals;
-          // vector<double> next_y_vals;
-          
+
+          // Polynomial evaluation
+          std::vector<double> xv_ref_traj;
+          std::vector<double> yv_ref_traj;
+
+          for (double x = 0; x <= 50; x += 5.0) {
+
+            auto v = poly.Eval(x);
+            xv_ref_traj.push_back(x);
+            yv_ref_traj.push_back(v);
+
+          }
+        
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
 
-          // msgJson["next_x"] = next_x_vals;
-          // msgJson["next_y"] = next_y_vals;
-          msgJson["next_x"] = ptsx_v;
-          msgJson["next_y"] = ptsy_v;
+          msgJson["next_x"] = xv_ref_traj;
+          msgJson["next_y"] = yv_ref_traj;
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           // std::cout << msg << std::endl;
@@ -174,7 +136,7 @@ int main() {
           //
           // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
           // SUBMITTING.
-          this_thread::sleep_for(chrono::milliseconds(100));
+          std::this_thread::sleep_for(std::chrono::milliseconds(100));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
