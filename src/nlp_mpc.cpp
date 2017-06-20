67bc9af1906cb5e6cc8fb921c1da62c8deba6967
dmbelo <david.belo@mclaren.com>
2017-06-19 23:31:06 -0700

    Parameterisation using json

--- src/nlp_mpc.cpp
+++ src/nlp_mpc.cpp
@@ -1,40 +1,23 @@
 #include <math.h>
+#include <fstream>
 #include <cppad/ipopt/solve.hpp>
 #include "Eigen-3.3/Eigen/Core"
 #include "Poly.h"
 #include "Car.h"
 #include "matplotlibcpp.h"
+#include "json.hpp"
 
 #define _USE_MATH_DEFINES
 #define VERY_LARGE_NUMBER 1.0e15
 
 using CppAD::AD;
 using std::vector;
+using json = nlohmann::json;
+
 namespace plt = matplotlibcpp;
-
-double t_horizon = 1;
-
-size_t n_states = 6;
-size_t n_steps = 10;
-size_t n_actuators = 2;
-size_t n_constraints = n_steps * n_states; 
-size_t n_vars = n_states * n_steps + n_actuators * (n_steps - 1);
-
-// The solver takes all the state variables and actuator
-// variables in a singular vector. Thus, we should to establish
-// when one variable starts and another ends to make our lifes easier.
-size_t idx_x_car = 0;
-size_t idx_y_car = idx_x_car + n_steps;
-size_t idx_psi_car = idx_y_car + n_steps;
-size_t idx_v_car = idx_psi_car + n_steps;
-size_t idx_cte_car = idx_v_car + n_steps;
-size_t idx_err_psi_car = idx_cte_car + n_steps;
-size_t idx_a_steer = idx_err_psi_car + n_steps;
-size_t idx_g_accel = idx_a_steer + n_steps - 1;
 
 double l_front = 2.67;
 double v_car_ref = 34;
-double dt = t_horizon / n_steps;
 
 double max_a_steer = 25.0 * M_PI / 180.0;
 double max_r_throttle = 1.0;
@@ -44,32 +27,80 @@ struct control{
     double g_accel;
 };
 
-struct mpc{
-    double cte;
-    double err_psi;
-    double err_v_car;
-    double mag_a_steer;
-    double mag_g_accel;
+struct params{
+    size_t n_steps;
+    size_t n_constraints;
+    size_t n_vars;
+    size_t n_states;
+    size_t n_actuators;
+    double t_horizon;
+    double dt;
 };
 
+struct index_map{
+    size_t x_car;
+    size_t y_car;
+    size_t psi_car;
+    size_t v_car;
+    size_t cte_car;
+    size_t err_psi_car;
+    size_t a_steer;
+    size_t g_accel;
+};
+
+template<typename Type>
+struct objective{
+    Type cte;
+    Type err_psi;
+    Type err_v_car;
+    Type mag_a_steer;
+    Type mag_g_accel;
+};
+
+template<typename Type>
 struct output{
     control u;
-    mpc m;
+    objective<Type> obj;
 };
+
+template <typename TypeX, typename TypeObj>
+TypeObj obj_fun(TypeX x, params &p, index_map &idx) {
+
+    TypeObj obj;
+
+    obj.cte = 0;
+    obj.err_psi = 0;
+    obj.err_v_car = 0;
+    for (size_t t = 0; t < p.n_steps; t++) {
+        obj.cte += 1e4 * pow(x[idx.cte_car + t], 2);
+        obj.err_psi += 1e5 * pow(x[idx.err_psi_car + t], 2);
+        obj.err_v_car += 1e0 * pow(x[idx.v_car + t] - v_car_ref, 2);
+    }
+
+    obj.mag_a_steer = 0;
+    obj.mag_g_accel = 0;
+    for (size_t t = 0; t < p.n_steps - 1; t++) {
+        obj.mag_a_steer += 1e1 * pow(x[idx.a_steer + t], 2);
+        obj.mag_g_accel += 1e1 * pow(x[idx.g_accel + t], 2);
+    }
+
+    return obj;
+
+}
+
 
 class FG_eval {
 
     public:
 
-        AD<double> cte;
-        AD<double> err_psi;
-        AD<double> err_v_car;
-        AD<double> mag_a_steer;
-        AD<double> mag_g_accel;
         Eigen::VectorXd coeffs;
+        params p;
+        index_map idx;
 
-        FG_eval(Eigen::VectorXd coeffs) {
+        FG_eval(Eigen::VectorXd coeffs, params p, index_map idx) {
             this->coeffs = coeffs;
+            this->p = p;
+            this->idx = idx;
         }
 
         typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
@@ -80,59 +111,65 @@ class FG_eval {
             // Objective function
             //******************************
         
-            cte = 0;
-            err_psi = 0;
-            err_v_car = 0;
-            for (size_t t = 0; t < n_steps; t++) {
-                cte += 1e4 * pow(x[idx_cte_car + t], 2);
-                err_psi += 1e5 * pow(x[idx_err_psi_car + t], 2);
-                err_v_car += 1e0 * pow(x[idx_v_car + t] - v_car_ref, 2);
-            }
+            // cte = 0;
+            // err_psi = 0;
+            // err_v_car = 0;
+            // for (size_t t = 0; t < n_steps; t++) {
+            //     cte += 1e4 * pow(x[idx_cte_car + t], 2);
+            //     err_psi += 1e5 * pow(x[idx_err_psi_car + t], 2);
+            //     err_v_car += 1e0 * pow(x[idx_v_car + t] - v_car_ref, 2);
+            // }
 
-            mag_a_steer = 0;
-            mag_g_accel = 0;
-            for (size_t t = 0; t < n_steps - 1; t++) {
-                mag_a_steer += 0e1 * pow(x[idx_a_steer + t], 2);
-                mag_g_accel += 0e1 * pow(x[idx_g_accel + t], 2);
-            }
+            // mag_a_steer = 0;
+            // mag_g_accel = 0;
+            // for (size_t t = 0; t < n_steps - 1; t++) {
+            //     mag_a_steer += 0e1 * pow(x[idx_a_steer + t], 2);
+            //     mag_g_accel += 0e1 * pow(x[idx_g_accel + t], 2);
+            // }
 
+            objective<AD<double>> obj;
+            obj = obj_fun<ADvector, objective<AD<double>>>(x, p, idx);
 
-            fg[0] = cte + err_psi + err_v_car + mag_a_steer + mag_g_accel;
+            fg[0] = obj.cte + 
+                    obj.err_psi + 
+                    obj.err_v_car + 
+                    obj.mag_a_steer + 
+                    obj.mag_g_accel;
 
             //******************************
             // Constraints
             //******************************
 
             // Initial conditions
-            fg[1 + idx_x_car] = x[idx_x_car];
-            fg[1 + idx_y_car] = x[idx_y_car];
-            fg[1 + idx_psi_car] = x[idx_psi_car];
-            fg[1 + idx_v_car] = x[idx_v_car];
-            fg[1 + idx_cte_car] = x[idx_cte_car];
-            fg[1 + idx_err_psi_car] = x[idx_err_psi_car];
+            fg[1 + idx.x_car] = x[idx.x_car];
+            fg[1 + idx.y_car] = x[idx.y_car];
+            fg[1 + idx.psi_car] = x[idx.psi_car];
+            fg[1 + idx.v_car] = x[idx.v_car];
+            fg[1 + idx.cte_car] = x[idx.cte_car];
+            fg[1 + idx.err_psi_car] = x[idx.err_psi_car];
 
             // Vehicle model constraints
-            for (size_t t = 1; t < n_steps; t++) {
+            for (size_t t = 1; t < p.n_steps; t++) {
 
                 // States at t
-                AD<double> x_car_0 = x[idx_x_car + t - 1];
-                AD<double> y_car_0 = x[idx_y_car + t - 1];
-                AD<double> psi_car_0 = x[idx_psi_car + t - 1];
-                AD<double> v_car_0 = x[idx_v_car + t - 1];
-                AD<double> cte_car_0 = x[idx_cte_car + t - 1];
-                AD<double> err_psi_car_0 = x[idx_err_psi_car + t - 1];
+                AD<double> x_car_0 = x[idx.x_car + t - 1];
+                AD<double> y_car_0 = x[idx.y_car + t - 1];
+                AD<double> psi_car_0 = x[idx.psi_car + t - 1];
+                AD<double> v_car_0 = x[idx.v_car + t - 1];
+                AD<double> cte_car_0 = x[idx.cte_car + t - 1];
+                AD<double> err_psi_car_0 = x[idx.err_psi_car + t - 1];
 
                 // States at t+1
-                AD<double> x_car_1 = x[idx_x_car + t];
-                AD<double> y_car_1 = x[idx_y_car + t];
-                AD<double> psi_car_1 = x[idx_psi_car + t];
-                AD<double> v_car_1 = x[idx_v_car + t];
-                AD<double> cte_car_1 = x[idx_cte_car + t];
-                AD<double> err_psi_car_1 = x[idx_err_psi_car + t];
+                AD<double> x_car_1 = x[idx.x_car + t];
+                AD<double> y_car_1 = x[idx.y_car + t];
+                AD<double> psi_car_1 = x[idx.psi_car + t];
+                AD<double> v_car_1 = x[idx.v_car + t];
+                AD<double> cte_car_1 = x[idx.cte_car + t];
+                AD<double> err_psi_car_1 = x[idx.err_psi_car + t];
 
                 // Actuators at t
-                AD<double> a_steer_0 = x[idx_a_steer + t - 1];
-                AD<double> r_throttle_0 = x[idx_g_accel + t - 1];
+                AD<double> a_steer_0 = x[idx.a_steer + t - 1];
+                AD<double> r_throttle_0 = x[idx.g_accel + t - 1];
 
                 CppAD::AD<double> x_car_0_squared = x_car_0 * x_car_0;
                 CppAD::AD<double> x_car_0_cubed = x_car_0_squared * x_car_0;
@@ -140,12 +177,12 @@ class FG_eval {
                 CppAD::AD<double> psi_ref_0 = atan(coeffs[1] + 2 * coeffs[2] * x_car_0 + 3 * coeffs[3] * x_car_0_squared);
 
                 // Setup the rest of the model constraints
-                fg[1 + idx_x_car + t] = x_car_1 - (x_car_0 + v_car_0 * cos(psi_car_0) * dt);
-                fg[1 + idx_y_car + t] = y_car_1 - (y_car_0 + v_car_0 * sin(psi_car_0) * dt);
-                fg[1 + idx_psi_car + t] = psi_car_1 - (psi_car_0 + v_car_0 / l_front * a_steer_0 * dt);
-                fg[1 + idx_v_car + t] = v_car_1 - (v_car_0 + r_throttle_0 * dt);
-                fg[1 + idx_cte_car + t] = cte_car_1 - (f_0 - y_car_0 + v_car_0 * sin(err_psi_car_0) * dt);
-                fg[1 + idx_err_psi_car + t] = err_psi_car_1 - (psi_car_0 - psi_ref_0 + v_car_0 / l_front * a_steer_0 * dt);
+                fg[1 + idx.x_car + t] = x_car_1 - (x_car_0 + v_car_0 * cos(psi_car_0) * p.dt);
+                fg[1 + idx.y_car + t] = y_car_1 - (y_car_0 + v_car_0 * sin(psi_car_0) * p.dt);
+                fg[1 + idx.psi_car + t] = psi_car_1 - (psi_car_0 + v_car_0 / l_front * a_steer_0 * p.dt);
+                fg[1 + idx.v_car + t] = v_car_1 - (v_car_0 + r_throttle_0 * p.dt);
+                fg[1 + idx.cte_car + t] = cte_car_1 - (f_0 - y_car_0 + v_car_0 * sin(err_psi_car_0) * p.dt);
+                fg[1 + idx.err_psi_car + t] = err_psi_car_1 - (psi_car_0 - psi_ref_0 + v_car_0 / l_front * a_steer_0 * p.dt);
 
             }
 
@@ -155,9 +192,9 @@ class FG_eval {
 };
 
 
-output solve(Eigen::VectorXd states, Eigen::VectorXd coeffs) {
+output<double> solve(Eigen::VectorXd states, Eigen::VectorXd coeffs, params p, index_map idx) {
 
-    output o; 
+    output<double> out; 
 
     bool ok = true;
     size_t i;
@@ -170,10 +207,10 @@ output solve(Eigen::VectorXd states, Eigen::VectorXd coeffs) {
     double cte_car = states[4];
     double err_psi_car = states[5];
 
-    Dvector x(n_vars);
-    Dvector xl(n_vars);
-    Dvector xu(n_vars);
-    for (i = 0; i < n_vars; i++) {
+    Dvector x(p.n_vars);
+    Dvector xl(p.n_vars);
+    Dvector xu(p.n_vars);
+    for (i = 0; i < p.n_vars; i++) {
         // Initial value of optimization variables
         x[i] = 0.0; 
         // Lower and upper limits of optimization variables
@@ -181,54 +218,54 @@ output solve(Eigen::VectorXd states, Eigen::VectorXd coeffs) {
         xu[i] = VERY_LARGE_NUMBER;
     }
 
-    x[idx_x_car] = x_car;
-    x[idx_y_car] = x_car;
-    x[idx_psi_car] = psi_car;
-    x[idx_v_car] = v_car;
-    x[idx_cte_car] = cte_car;
-    x[idx_err_psi_car] = err_psi_car;
+    x[idx.x_car] = x_car;
+    x[idx.y_car] = x_car;
+    x[idx.psi_car] = psi_car;
+    x[idx.v_car] = v_car;
+    x[idx.cte_car] = cte_car;
+    x[idx.err_psi_car] = err_psi_car;
 
-    // for (i = idx_v_car; i < idx_cte_car; i++) {
+    // for (i = idx.v_car; i < idx.cte_car; i++) {
     //     xl[i] = -max_v_car;
     //     xu[i] = max_v_car;
     // }
 
-    for (i = idx_a_steer; i < idx_g_accel; i++) {
+    for (i = idx.a_steer; i < idx.g_accel; i++) {
         xl[i] = -max_a_steer;
         xu[i] = max_a_steer;
     }
 
-    for (i = idx_g_accel; i < n_vars; i++) {
+    for (i = idx.g_accel; i < p.n_vars; i++) {
         xl[i] = -max_r_throttle;
         xu[i] = max_r_throttle;
     }
 
     // Lower and upper constraints bounds should be zero except for initial 
     // conditions
-    Dvector gl(n_constraints);
-    Dvector gu(n_constraints);
-    for (i = 0; i < n_constraints; i++) {
+    Dvector gl(p.n_constraints);
+    Dvector gu(p.n_constraints);
+    for (i = 0; i < p.n_constraints; i++) {
         // Lower and upper constraint bounds on optimization variables
         gl[i] = 0;
         gu[i] = 0;
     }
 
-    gl[idx_x_car] = x_car;
-    gl[idx_y_car] = y_car;
-    gl[idx_psi_car] = psi_car;
-    gl[idx_v_car] = v_car;
-    gl[idx_cte_car] = cte_car;
-    gl[idx_err_psi_car] = err_psi_car;
+    gl[idx.x_car] = x_car;
+    gl[idx.y_car] = y_car;
+    gl[idx.psi_car] = psi_car;
+    gl[idx.v_car] = v_car;
+    gl[idx.cte_car] = cte_car;
+    gl[idx.err_psi_car] = err_psi_car;
 
-    gu[idx_x_car] = x_car;
-    gu[idx_y_car] = y_car;
-    gu[idx_psi_car] = psi_car;
-    gu[idx_v_car] = v_car;
-    gu[idx_cte_car] = cte_car;
-    gu[idx_err_psi_car] = err_psi_car;
+    gu[idx.x_car] = x_car;
+    gu[idx.y_car] = y_car;
+    gu[idx.psi_car] = psi_car;
+    gu[idx.v_car] = v_car;
+    gu[idx.cte_car] = cte_car;
+    gu[idx.err_psi_car] = err_psi_car;
 
     // Object that computes objective and constraints
-    FG_eval fg_eval(coeffs);
+    FG_eval fg_eval(coeffs, p, idx);
 
     // options
     std::string options;
@@ -255,25 +292,56 @@ output solve(Eigen::VectorXd states, Eigen::VectorXd coeffs) {
     ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
 
     // Evaluate constraints with solution
-    // fg_eval(solution.x);
+    objective<double> obj;
+    obj = obj_fun<Dvector, objective<double>>(solution.x, p, idx);
 
-    o.u.a_steer = solution.x[idx_a_steer];
-    o.u.g_accel = solution.x[idx_g_accel];
-    o.m.cte = Value(fg_eval.cte);
-    o.m.err_psi = Value(fg_eval.err_psi);
-    o.m.err_v_car = Value(fg_eval.err_v_car);
-    o.m.mag_a_steer = Value(fg_eval.mag_a_steer);
-    o.m.mag_g_accel = Value(fg_eval.mag_g_accel);
+    out.u.a_steer = solution.x[idx.a_steer];
+    out.u.g_accel = solution.x[idx.g_accel];
+    out.obj.cte = obj.cte;
+    out.obj.err_psi = obj.err_psi;
+    out.obj.err_v_car = obj.err_v_car;
+    out.obj.mag_a_steer = obj.mag_a_steer;
+    out.obj.mag_g_accel = obj.mag_g_accel;
 
-    return o;
+    return out;
 
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    // Read json file with params
+    std::ifstream file;
+    file.open(argv[1]);
+    json user_params;
+    file >> user_params;
+
+    params p;
+
+    p.t_horizon = 1;
+    p.n_steps = 10;
+    p.dt = p.t_horizon / p.n_steps;
+    p.n_states = 6;
+    p.n_actuators = 2;
+    p.n_constraints = p.n_steps * p.n_states; 
+    p.n_vars = p.n_states * p.n_steps + p.n_actuators * (p.n_steps - 1);
+
+    // The solver takes all the state variables and actuator
+    // variables in a singular vector. Thus, we should to establish
+    // when one variable starts and another ends to make our lifes easier.
+    
+    index_map idx;
+    idx.x_car = 0;
+    idx.y_car = idx.x_car + p.n_steps;
+    idx.psi_car = idx.y_car + p.n_steps;
+    idx.v_car = idx.psi_car + p.n_steps;
+    idx.cte_car = idx.v_car + p.n_steps;
+    idx.err_psi_car = idx.cte_car + p.n_steps;
+    idx.a_steer = idx.err_psi_car + p.n_steps;
+    idx.g_accel = idx.a_steer + p.n_steps - 1;
 
     Poly poly;
-    output o;
+    output<double> o;
 
     Eigen::VectorXd x_waypts(8); 
     Eigen::VectorXd y_waypts(8); 
@@ -315,9 +383,9 @@ int main() {
         // Update state vector
         car.cte = (poly.Eval(car.x) - car.y) * cos(car.psi);
         car.err_psi = car.psi - atan(poly.Diff(car.x));
-
         states << car.x, car.y, car.psi, car.v, car.cte, car.err_psi;
-        o = solve(states, poly.coeffs);
+
+        o = solve(states, poly.coeffs, p, idx);
         car.Step(o.u.a_steer, o.u.g_accel, dt_sim);
         
         // Logging
@@ -327,11 +395,11 @@ int main() {
         v_car.push_back(car.v);
         a_steer.push_back(o.u.a_steer);
         g_accel.push_back(o.u.g_accel);
-        cte.push_back(o.m.cte);
-        err_psi.push_back(o.m.err_psi);
-        err_v_car.push_back(o.m.err_v_car);
-        mag_a_steer.push_back(o.m.mag_a_steer);
-        mag_g_accel.push_back(o.m.mag_g_accel); 
+        cte.push_back(o.obj.cte);
+        err_psi.push_back(o.obj.err_psi);
+        err_v_car.push_back(o.obj.err_v_car);
+        mag_a_steer.push_back(o.obj.mag_a_steer);
+        mag_g_accel.push_back(o.obj.mag_g_accel); 
 
     }
 
@@ -362,20 +430,23 @@ int main() {
     // y_car_.resize(y_car.size());
     // Eigen::VectorXd::Map(&y_car_[0], y_car.size()) = y_car;
 
-    plt::subplot(4,1,1);
+    plt::subplot(5,1,1);
     plt::plot(xw, yw, "o");
     plt::plot(xf, yf);
     plt::plot(x_car, y_car, "-*");
 
-    plt::subplot(4,1,2);
+    plt::subplot(5,1,2);
     plt::named_plot("vCar", v_car);
 
-    plt::subplot(4,1,3);
+    plt::subplot(5,1,3);
     plt::named_plot("aSteer", a_steer);
+    plt::legend();
+
+    plt::subplot(5,1,4);
     plt::named_plot("gAccel", g_accel);
     plt::legend();
 
-    plt::subplot(4,1,4);
+    plt::subplot(5,1,5);
     plt::named_plot("CTE", cte);
     plt::named_plot("Err aCar", err_psi);
     plt::named_plot("Err vCar", err_v_car, "-*");
